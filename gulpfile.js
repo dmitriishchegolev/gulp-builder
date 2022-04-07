a7151c2e3445702674163b38bbcb17872c6080fc
dmitriishchegolev <dmitriishchegolev@yandex.ru>
2022-04-07 21:07:59 +0300

    Реализовал деплой

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ import gulp from 'gulp';
 import { buildProcessDev, buildProcessProd } from './tasks/build.js';
 import { server } from './tasks/server.js';
 import { watchDev, watchProd } from './tasks/watch.js';
+import { uploadToS3 } from './tasks/s3.js';
 
 export const buildDev = buildProcessDev;
 export const buildProd = buildProcessProd;
@@ -14,3 +15,4 @@ export const serveProd = gulp.series(
   buildProcessProd,
   gulp.parallel(server, watchProd)
 );
+export const s3Upload = uploadToS3;
