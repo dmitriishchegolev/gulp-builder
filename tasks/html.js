0bbdf64023862e80a741ebc33581587da91f4983
dmitriishchegolev <dmitriishchegolev@yandex.ru>
2022-04-07 23:52:21 +0300

    Доработал сборку html

--- tasks/html.js
+++ tasks/html.js
@@ -1,19 +1,28 @@
 import gulp from 'gulp';
 import fileinclude from 'gulp-file-include';
 import htmlmin from 'gulp-htmlmin';
+import rename from 'gulp-rename';
 import connect from 'gulp-connect';
 
 export const buildHtmlDev = () =>
   gulp
-    .src(['src/app/**/*.html', '!src/app/modules/**/*.html'])
+    .src([
+      'src/app/modules/**/*.html',
+      '!src/app/modules/**/components/**/*.html',
+    ])
     .pipe(fileinclude())
+    .pipe(rename({ dirname: '' }))
     .pipe(gulp.dest('./dist'))
     .pipe(connect.reload());
 
 export const buildHtmlProd = () =>
   gulp
-    .src(['src/app/**/*.html', '!src/app/modules/**/*.html'])
+    .src([
+      'src/app/modules/**/*.html',
+      '!src/app/modules/**/components/**/*.html',
+    ])
     .pipe(fileinclude())
+    .pipe(rename({ dirname: '' }))
     .pipe(htmlmin({ collapseWhitespace: true }))
     .pipe(gulp.dest('./dist'))
     .pipe(connect.reload());
