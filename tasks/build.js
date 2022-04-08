c0edcfe1f1fffd33e5f99341e6ac60266b24366c
dmitriishchegolev <dmitriishchegolev@yandex.ru>
2022-04-08 13:04:51 +0300

    Переработал билд ассетов

--- tasks/build.js
+++ tasks/build.js
@@ -3,8 +3,7 @@ import { buildHtmlDev, buildHtmlProd } from './html.js';
 import { buildStylesDev, buildStylesProd } from './styles.js';
 import { buildJsDev, buildJsProd } from './js.js';
 import { cleanDist } from './clean.js';
-
-const buildAssets = () => gulp.src('src/assets').pipe(gulp.dest('./dist'));
+import { buildAssets } from './assets.js';
 
 export const buildProcessDev = gulp.series(
   cleanDist,
